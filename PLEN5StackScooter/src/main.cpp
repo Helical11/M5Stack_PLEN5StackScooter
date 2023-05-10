b85d0fb125d9747117f7b4d517c0aaf82164ac51
Helical11 <helical@helical-engineer.com>
2023-05-10 21:08:01 +0900

    [Add]IMUを使用したコードテスト

--- PLEN5StackScooter/src/main.cpp
+++ PLEN5StackScooter/src/main.cpp
@@ -2,13 +2,19 @@
 
 void setup() {
   M5.begin(); // Initialize M5StackAtomS3
-  M5.Lcd.clear(); // Clear the display
+  M5.Imu.begin(); // Initialize the IMU sensor
   M5.Lcd.setTextSize(2); // Set the text size to 2
   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK); // Set the text color to white on black
-  M5.Lcd.setCursor(10, 20); // Set the cursor position
-  M5.Lcd.print("HelloWorld"); // Print "HelloWorld" on the display
 }
 
+float ax, ay, az, gx, gy, gz, t;
+
 void loop() {
-  // Empty loop
+  M5.Lcd.clear(); // 画面をクリア
+  M5.Imu.getAccel(&ax, &ay, &az);
+  M5.Imu.getGyro(&gx, &gy, &gz);
+  float zAccel = az / 1000.0; // Z軸方向の加速度をmgからgに変換
+  M5.Lcd.setCursor(10, 20); // カーソル位置を設定
+  M5.Lcd.printf("Z-Accel: %.2fg", az); // Z軸方向の加速度の値を表示
+  delay(100); // 100ミリ秒待つ
 }
