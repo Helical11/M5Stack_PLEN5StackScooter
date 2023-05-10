c5ccb4c7c85968f316c38f8cc0454721c014ad20
Helical11 <helical@helical-engineer.com>
2023-05-10 20:44:31 +0900

    [Add]HelloWorld

--- PLEN5StackScooter/src/main.cpp
+++ PLEN5StackScooter/src/main.cpp
@@ -1,9 +1,14 @@
 #include <M5Unified.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin(); // Initialize M5StackAtomS3
+  M5.Lcd.clear(); // Clear the display
+  M5.Lcd.setTextSize(2); // Set the text size to 2
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK); // Set the text color to white on black
+  M5.Lcd.setCursor(10, 20); // Set the cursor position
+  M5.Lcd.print("HelloWorld"); // Print "HelloWorld" on the display
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Empty loop
 }
